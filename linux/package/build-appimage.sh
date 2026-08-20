80a093b759deb514ab6a63af921258e7c59f85bb
Alectardy98 <61422584+Alectardy98@users.noreply.github.com>
2026-08-20 01:55:07 -0400

    fix icon

--- linux/package/build-appimage.sh
+++ linux/package/build-appimage.sh
@@ -19,8 +19,8 @@ dotnet publish QMKToolbox/QMKToolbox.csproj -c Release -r linux-x64 --self-conta
 
 mkdir -p AppDir/usr/bin AppDir/usr/share/applications AppDir/usr/share/icons/hicolor/256x256/apps
 cp -a dist/. AppDir/usr/bin/
-cp package/qmk-toolbox.desktop AppDir/usr/share/applications/qmk-toolbox.desktop
-cp package/qmk-toolbox.desktop AppDir/qmk-toolbox.desktop
+cp package/QMKToolbox.desktop AppDir/usr/share/applications/QMKToolbox.desktop
+cp package/QMKToolbox.desktop AppDir/QMKToolbox.desktop
 
 python -c 'from PIL import Image; img=Image.open("QMKToolbox/Resources/qmk.ico"); img.seek(getattr(img,"n_frames",1)-1); img=img.convert("RGBA"); img.thumbnail((256,256)); img.save("AppDir/qmk-toolbox.png"); img.save("AppDir/usr/share/icons/hicolor/256x256/apps/qmk-toolbox.png")'
 
